7a36ea15e30083880a34edfd49340f34e36d35c4
Morten Kristensen <me@mortens.dev>
2021-05-14 21:58:59 +0200

    Faction API endpoint

--- torn_api.py
+++ torn_api.py
@@ -16,7 +16,7 @@ Fields: ammo, attacks, attacksfull, bars, basic, battlestats, bazaar, cooldowns,
 display, education, events, gym, hof, honors, icons, inventory, jobpoints, log, medals, merits,
 messages, money, networth, notifications, perks, personalstats, profile, properties, receivedevents,
 refills, reports, revives, revivesfull, skills, stocks, timestamp, travel, weaponexp, workstats
-  """
+    """
     fields = fields or []
     params = {
       "key": self.__key,
@@ -38,7 +38,7 @@ refills, reports, revives, revivesfull, skills, stocks, timestamp, travel, weapo
 If property ID is unspecified, the property related to the API key is used.
 
 Fields: property, timestamp
-  """
+    """
     fields = fields or []
     params = {
       "key": self.__key,
@@ -47,5 +47,32 @@ Fields: property, timestamp
     url = "{}/property/".format(self.__base_url)
     if property_id:
       url += property_id
+    resp = requests.get(url, params=params)
+    return resp.json()
+
+  def faction(self, faction_id=None, fields=None, since=None, until=None):
+    """Fetch faction information.
+
+If faction ID is unspecified, the faction related to the API key is used.
+
+UNIX epoch timestamps `since` and `until` work for some fields.
+
+Fields: applications, armor, armorynews, attacknews, attacks, attacksfull, basic, boosters, cesium,
+chain, chains, contributors, crimenews, crimes, currency, donations, drugs, fundsnews, mainnews,
+medical, membershipnews, reports, revives, revivesfull, stats, temporary, territory, timestamp,
+upgrades, weapons
+    """
+    fields = fields or []
+    params = {
+      "key": self.__key,
+      "selections": ",".join(fields),
+    }
+    if since:
+      params["from"] = str(int(since))
+    if until:
+      params["to"] = str(int(until))
+    url = "{}/faction/".format(self.__base_url)
+    if faction_id:
+      url += faction_id
     resp = requests.get(url, params=params)
     return resp.json()
